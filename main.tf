8e150f12a6035587b5addd614aa7fcd2b0c433fb
Renata Brasil <rebra@example.com>
2022-10-15 18:27:41 -0300

    adjusmtns in repo workflows

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
-# The configuration for the `remote` backend.
 terraform {
   backend "remote" {
-    # The name of your Terraform Cloud organization.
     organization = "renata-corporation"
-
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-      aws = {
-        source = "hashicorp/aws"
-      }
+      name = "Example-Workspace"
+    }
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
     }
   }
 
