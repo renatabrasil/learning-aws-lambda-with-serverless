306cb38f3ba399e1147b10344d61b3692dc466ee
Renata Brasil <rebra@example.com>
2022-10-15 21:41:22 -0300

    create products endpoint

--- dynamodb.tf
+++ dynamodb.tf
@@ -26,15 +26,15 @@ resource "aws_dynamodb_table" "productsdb" {
     enabled        = false
   }
 
-#  global_secondary_index {
-#    name               = "GameTitleIndex"
-#    hash_key           = "GameTitle"
-#    range_key          = "TopScore"
-#    write_capacity     = 10
-#    read_capacity      = 10
-#    projection_type    = "INCLUDE"
-#    non_key_attributes = ["UserId"]
-#  }
+  #  global_secondary_index {
+  #    name               = "GameTitleIndex"
+  #    hash_key           = "GameTitle"
+  #    range_key          = "TopScore"
+  #    write_capacity     = 10
+  #    read_capacity      = 10
+  #    projection_type    = "INCLUDE"
+  #    non_key_attributes = ["UserId"]
+  #  }
 
   tags = {
     Name        = "dynamodb-table-1"
